1577fa43ebc071693507dc7cf6bc65e812bc426c
Jesús Rodriguez <jesusrodriguez@Jesuss-MacBook-Air-2.local>
2026-02-19 20:40:44 -0600

    Implementacion de problema de Sum Two

--- backend/app.js
+++ backend/app.js
@@ -17,14 +17,39 @@ function texto(str){
     filesystem.writeFileSync('node.txt' , str)
 }
 
+function problema_TwoSum (nums, target) {
+    respuesta = [];
+    for(let i = 0; i < nums.length; i++){
+        let j = i + 1;
+        while(j != i){
+            if((nums[i] + nums[j]) == target){
+                respuesta = [i, j];
+                return respuesta;
+            }
+            j = (j + 1) % nums.length; 
+        }
+    }
+    return respuesta;
+}
+
 const http = require('http');
 
 const server = http.createServer ( (request, response ) => {
     console.log(request.url);
+    // 1
     arr = [13,43,56,34];
     console.log(arreglo(arr));
+
+    // 2
     str = "Buentas tardes";
     texto(str);
+
+    // problema mio
+    nums = [3,7,2,8,6,1]
+    target = 10;
+    respuesta = problema_TwoSum(nums, target);
+    console.log(nums[respuesta[0]] + " + " + nums[respuesta[1]] + " = " + target);
+
     response.end();
 })
 
