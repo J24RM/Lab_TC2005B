9d4fc736e6daf8c387e35b494236834840fc3ea9
Jesús Rodriguez <jesusrodriguez@Jesuss-MacBook-Air-2.local>
2026-02-23 13:00:48 -0600

    FEAT: Se agrego tres rutas mas diferentes

--- backend/app.js
+++ backend/app.js
@@ -33,61 +33,6 @@ function problema_TwoSum (nums, target) {
 }
 
 const html = `
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Pokemon</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
-  <style>
-    .is-purple {
-      background-color: #a64ac9;
-      color: white;
-    }
-  </style>
-</head>
-
-<body>
-  <div class="main-content">
-    <section class="section">
-      <div class="container">
-
-        <section class="hero">
-          <div class="hero-body">
-            <figure class="image is-128x128 is-inline-block">
-              <img class="is-rounded" src="https://i.pinimg.com/474x/8a/e5/08/8ae50860c9392e73de6b68c016a222c9.jpg">
-            </figure>
-            <p class="title">POKEMON</p>
-          </div>
-        </section>
-
-        <nav class="navbar" role="navigation">
-          <div class="navbar-menu">
-            <div class="navbar-start">
-              <a href="https://www.pokemon.com/el" class="navbar-item">
-                Página Oficial
-              </a>
-
-              <a href="https://www.pokemon.com/el/videojuegos-pokemon" class="navbar-item">
-                Videojuegos
-              </a>
-
-              <div class="navbar-item has-dropdown is-hoverable">
-                <a class="navbar-link">Más</a>
-                <div class="navbar-dropdown">
-                  <a href="https://www.pokemon.com/el/jcc-pokemon" class="navbar-item">
-                    TCJ
-                  </a>
-                  <a href="https://www.pokemon.com/el/dibujos-animados" class="navbar-item">
-                    Anime
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </nav>
-
         <div>
           <button class="button" onclick="shiny()"> Shiny </button>
         </div>
@@ -220,8 +165,70 @@ const html = `
       </div>
     </section>
   </div>
+`;
+const html_header = `
+  <!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Pokemon</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
+  <style>
+    .is-purple {
+      background-color: #a64ac9;
+      color: white;
+    }
+  </style>
+</head>
 
-  <footer class="footer">
+<body>
+  <div class="main-content">
+    <section class="section">
+      <div class="container">
+
+        <section class="hero">
+          <div class="hero-body">
+            <figure class="image is-128x128 is-inline-block">
+              <img class="is-rounded" src="https://i.pinimg.com/474x/8a/e5/08/8ae50860c9392e73de6b68c016a222c9.jpg">
+            </figure>
+            <p class="title">POKEMON</p>
+          </div>
+        </section>
+
+        <nav class="navbar" role="navigation">
+          <div class="navbar-menu">
+            <div class="navbar-start">
+              <a href="https://www.pokemon.com/el" class="navbar-item">
+                Página Oficial
+              </a>
+
+              <a href="https://www.pokemon.com/el/videojuegos-pokemon" class="navbar-item">
+                Videojuegos
+              </a>
+
+              <a href = "http://localhost:3000/new" class = "navbar-item">
+                Nuevo Pokemon
+              </a>
+
+              <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">Más</a>
+                <div class="navbar-dropdown">
+                  <a href="https://www.pokemon.com/el/jcc-pokemon" class="navbar-item">
+                    TCJ
+                  </a>
+                  <a href="https://www.pokemon.com/el/dibujos-animados" class="navbar-item">
+                    Anime
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+`;
+
+const html_footer = `
+    <footer class="footer">
     <div class="content has-text-centered">
       <p class="title is-5">¿Qué es Material Design?</p>
       <p>
@@ -271,40 +278,71 @@ const html = `
   <script src="/Users/jesusrodriguez/Desktop/Repos/Lab_TC2005B/lab5-6/pokescript.js"></script>
 </body>
 </html>
-`
+`;
+
+const html_form = `
+  <form action="/new" method="post">
+  <div class="field">
+    <label for="numero" class="label"> Numero de Pokedex </label>
+    <div class="control">
+      <input id="numero class="input" type="number" placeholder="e.g 025">
+    </div>
+
+  </div><div class="field">
+    <label for="" class="label"> Nombre </label>
+    <div class="control">
+      <input id="nombre" class="input" type="text" placeholder="e.g Pikachu">
+    </div>
+  </div>
+
+  <div class="field">
+    <label for="tipo" class="label"> Tipo(s) </label>
+    <div class="control">
+      <input id="tipo" class="input" type="text" placeholder="e.g Electrico">
+    </div>
+  </div>
+
+  <div class="field">
+    <label for="region" class="label"> Region </label>
+    <div class="control">
+      <input id="region" class="input" type="text" placeholder="e.g Kanto">
+    </div>
+  </div>
+
+  <div class="field">
+    <label for="imagen" class="label">Imagen</label>
+    <div class="control">
+      <input id="imagen" class="input" type="text" placeholder="e.g. https://tooys.mx/media/catalog/product/cache/0daeb07bb1d294c1f281fab47369d56a/h/o/hot-toys-league-of-legends-vi-sixth-scale_0.jpg">
+    </div>
+  </div>
+
+  <input class="button is-primary" type="submit" value="Guardar personaje">
+</form>
+`;
 
 const http = require('http');
 
 const server = http.createServer ( (request, response ) => {
-    console.log(request.url);
-    // 1
-    arr = [13,43,56,34];
-    console.log(arreglo(arr));
-
-    // 2
-    str = "Buentas tardes";
-    texto(str);
-
-    // problema mio
-    nums = [3,7,2,8,6,1]
-    target = 10;
-    respuesta = problema_TwoSum(nums, target);
-    console.log(nums[respuesta[0]] + " + " + nums[respuesta[1]] + " = " + target);
-
-    // Mi pagina Propia
-    if(request.url == "/mi_pagina"){
-        response.setHeader('Content-Type', 'text/html');
-        response.write(html);
-        response.end();
-    }
-
-    // Mandar Respuesta
-    if(request.url == "/"){
-        response.setHeader('Content-Type', 'text/html');
-
-        response.write("HOLA");
-        response.end();
-    }
+  if(request.url == "/"){
+    response.setHeader('Content-Type', 'text/html');
+    response.write(html_header + html + html_footer);
+    response.end();
+  }
+  else if(request.url == "/new" && request.method == "GET"){
+    response.setHeader('Content-Type', 'text/html');
+    response.write(html_header + html_form + html_footer);
+    response.end();
+  }
+  else if(request.url == "/new" && request.method == "POST" ){
+    response.setHeader('Content-Type', 'text/html');
+    response.write(html_header + html + html_footer);
+    response.end();
+  }
+  else{
+    response.setHeader('Content-Type', 'text/html');
+    response.write("404");
+    response.end();
+  }
 
 })
 
